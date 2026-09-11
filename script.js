1740cfc950fd8a694efe30d12fd0073d7c36f0d3
srrys <109410@psdschools.org>
2026-09-10 21:16:27 -0600

    Correct element retrieval and update title content
    Fix typo in getElementById and update innerHTML.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function climatePush() {
-  document.getElemetnById("amap").innerSrc
+  document.getElemetnById("amap").src = "map2.png";
+  document.getElementById("titlecard").innerHTML = "The U.S post blog: climate map";
 }
