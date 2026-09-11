27b5ae77365710852f77ce30c60196e82938964c
srrys <109410@psdschools.org>
2026-09-11 10:20:44 -0600

    Fix typo in getElementById function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function climatePush() {
-  document.getElemetnById("amap").src = "map2.png";
+  document.getElementById("amap").src = "map2.png";
   document.getElementById("titlecard").innerHTML = "The U.S post blog: climate map";
 }
